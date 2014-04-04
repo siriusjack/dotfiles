69dd345ee9ae9ee312fa384db6991d07c8e9c461
Yoshihiro Hori <Yoshihiro@Yoshihiros-MacBook-Air.local>
2014-04-05 03:44:24 +0900

    add .zshrc

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc .zshrc)
 
 
-DOT_FILES=( .bashrc .bash.alias .tmux.conf .vim .vimrc .pythonrc.py)
+DOT_FILES=( .bashrc .bashrc.alias .tmux.conf .vim .vimrc .pythonrc.py .zshrc)
 
 for file in ${DOT_FILES[@]}
 do
