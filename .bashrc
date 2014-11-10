dd8c82475310a3795f6f47e1327ff17b3729fae0
siriusjack <siirusjack@gmail.com>
2014-11-10 14:34:23 +0900

    mod: emacs

--- .bashrc
+++ .bashrc
@@ -2,10 +2,4 @@
 [[ -s ~/.bashrc.alias ]] && source ~/.bashrc.alias
 [[ -s ~/.bashrc.local ]] && source ~/.bashrc.local
 [[ -s ~/.bashrc.path  ]] && source ~/.bashrc.path
-
-alias ls='ls -G'
-alias ll='ls -l'
-alias la='ls -a'
-alias del='gmv -f --backup=numbered --target-directory ~/.Trash'
-
-export PYTHONSTARTUP=~/.pythonrc.py
+# export PYTHONSTARTUP=~/.pythonrc.py
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/")) ;; add Melpa
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) ;; Marmalade
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/")) ;;
+(package-initialize) ;; init
+
 ; Japanese (UTF-8)
 (setq default-input-method "MacOSX")
 (set-language-environment 'Japanese)
@@ -9,3 +15,9 @@
 ; set default directory as ~/
 (setq default-directory "~/")
 (setq command-line-default-directory "~/")
+; Cmd as Meta key
+(setq ns-command-modifier (quote meta))
+(setq ns-alternate-modifier (quote super))
+
+(helm-mode 1)
+(define-key global-map (kbd "M-x") 'helm-M-x)
