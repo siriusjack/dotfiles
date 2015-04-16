50f7f0d26fce8f4d6d85aa06bc9cda821df0d81a
siriusjack <siriusjack@gmail.com>
2015-04-16 13:29:06 +0900

    Add getdate command

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -21,6 +21,12 @@ alias -s CONFIG=/Applications/Aten.app/Contents/MacOS/Aten
 function getpath() {
     pwd | tr -d '\n' | pbcopy
 }
+function getdatetime() {
+    date +%Y-%m-%d\ %H.%M.%S | tr -d '\n' | pbcopy
+}
+function getdate() {
+    date +%Y-%m-%d | tr -d '\n' | pbcopy
+}
 
 # ----- tmux -----
 autoload -Uz add-zsh-hook
