39341ba0a12a596801760375492011c3bc87d2c1
siriusjack <siriusjack@gmail.com>
2015-07-20 13:01:52 -0400

    Fix

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -7,16 +7,19 @@ if [ -d /usr/local/share/zsh-completions ]; then
     fpath=(/usr/local/share/zsh-completions $fpath)
 fi
 
+autoload -U compinit
+compinit
+# colors
+export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} 
+# match
 zstyle ':completion:*' matcher-list '' \
   'm:{a-z\-}={A-Z\_}' \
   'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
   'r:[[:ascii:]]||[[:ascii:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
-zstyle ':completion:*' menu select=2 
+# select
+zstyle ':completion:*:default' menu select=2 
 zstyle ':completion:*' list-separator '-->'
 zstyle ':completion:*:manuals' separate-sections true
-
-autoload -U compinit
-compinit
 
 typeset -U fpath FPATH
