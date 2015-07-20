84e6389b77839a417917a1e4f5516bb789b08650
siriusjack <siriusjack@gmail.com>
2015-07-20 11:37:37 -0400

    Add compinit

--- .zsh/.zshrc
+++ .zsh/.zshrc
@@ -25,10 +25,11 @@ setopt nobeep
 setopt nolistbeep
 # Emacs keybind (Supergeil XD!)
 bindkey -e
-# ls colors
+# colors
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+# colors for completion
 export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} 
+
 
 # lprompt
 # [user@host]
@@ -75,6 +76,6 @@ fi
 #_load_library $ZDOTDIR/plugins/zsh-python-prompt.zsh
 
 # Profiling
-#if (which zprof > /dev/null) ;then
-#  zprof | less
-#fi
+# if (which zprof > /dev/null) ;then
+#   zprof | less
+# fi
--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -7,9 +7,16 @@ if [ -d /usr/local/share/zsh-completions ]; then
     fpath=(/usr/local/share/zsh-completions $fpath)
 fi
 
-# Smart autocompletion
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} 
 zstyle ':completion:*' matcher-list '' \
   'm:{a-z\-}={A-Z\_}' \
   'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
-  'r:[[:ascii:]]||[[:ascii:]]=** r:|=* m:{a-z\-}={A-Z\_}'
+  'r:[[:ascii:]]||[[:ascii:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
+zstyle ':completion:*' menu select=2 
+zstyle ':completion:*' list-separator '-->'
+zstyle ':completion:*:manuals' separate-sections true
+
+autoload -U compinit
+compinit
+
 typeset -U fpath FPATH
