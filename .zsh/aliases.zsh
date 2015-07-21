ca859ef734f3e63ae7b10718b618e25bd38eb0f0
siriusjack <siriusjack@gmail.com>
2015-07-21 15:21:56 -0400

    Fix

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -97,8 +97,7 @@ function do_enter() {
     fi
     ls_abbrev
     if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
-        echo
-        echo -e "\e[0;33m--- git status ---\e[0m"
+        echo -e "\e[0;33m ----- git status -----\e[0m"
         git status -sb
         echo ""
     fi
