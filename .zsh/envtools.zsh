ec9abcbba63616f62c45411917053d1a39fffc79
siriusjack <siriusjack@gmail.com>
2015-08-25 20:26:54 -0400

    Fix

--- .zsh/envtools.zsh
+++ .zsh/envtools.zsh
@@ -14,16 +14,16 @@ if [ -d $HOME/.rbenv ]; then
     fi
 fi
 if [ -d $HOME/.nodebrew ]; then
-    if [ $uname != 'Darwin' ]; then
-        export PATH=$HOME/.nodebrew/current/bin:$PATH
-    fi
+    export PATH=$HOME/.nodebrew/current/bin:$PATH
 fi
 
 # init env tools
 if _command_exists pyenv; then
     eval "$(pyenv init -)"
     if [ -d $PYENV_ROOT/plugins/pyenv-virtualenv ]; then
-    	eval "$(pyenv virtualenv-init -)"
+        eval "$(pyenv virtualenv-init -)"
+    elif _command_exists pyenv-virtualenv; then
+        eval "$(pyenv virtualenv-init -)"
     fi
 fi
 if _command_exists rbenv; then
