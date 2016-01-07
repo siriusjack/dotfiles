28e2955b960364dba2fdfe4d2b74124ec39e9ca2
siriusjack <siriusjack@gmail.com>
2016-01-08 01:34:41 +0900

    Added GOPATH

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,8 +1,18 @@
 # path.zsh
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+# local
 if [ -d $HOME/local/bin ]; then
     export PATH=$HOME/local/bin:$PATH
 fi
+# user
 if [ -d $HOME/bin ]; then
     export PATH=$HOME/bin:$PATH
 fi
+
+# GO
+export GOPATH=$HOME/go
+if [ -d $GOPATH ]; then
+    export GOBIN=$GOPATH/bin
+    export PATH=$PATH:$GOPATH/bin
+fi
+
