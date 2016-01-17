9a7157dde978832349061cb0b92785f932dc9c4b
siriusjack <siriusjack@gmail.com>
2016-01-18 03:58:32 +0900

    Add direnv

--- .zsh/envtools.zsh
+++ .zsh/envtools.zsh
@@ -1,23 +1,23 @@
 # .zsh/env_tools.zsh
 local uname=`uname`
 
-# load path 
+# direnv
+if _command_exists direnv; then
+    eval "$(direnv hook zsh)"
+fi
+
+# python [pythonz|pyenv]
+# pythonz
+if [ -s $HOME/.pythonz/etc/bashrc ]; then
+    source $HOME/.pythonz/etc/bashrc
+fi
+# pyenv
 if [ -d $HOME/.pyenv ]; then
     if [ $uname != 'Darwin' ]; then
         export PYENV_ROOT=$HOME/.pyenv
         export PATH=$PYENV_ROOT/bin:$PATH
     fi
 fi
-if [ -d $HOME/.rbenv ]; then
-    if [ $uname != 'Darwin' ]; then
-        export PATH=$HOME/.rbenv/bin:$PATH
-    fi
-fi
-if [ -d $HOME/.nodebrew ]; then
-    export PATH=$HOME/.nodebrew/current/bin:$PATH
-fi
-
-# init env tools
 if _command_exists pyenv; then
     eval "$(pyenv init -)"
     if [ -d $PYENV_ROOT/plugins/pyenv-virtualenv ]; then
@@ -26,7 +26,25 @@ if _command_exists pyenv; then
         eval "$(pyenv virtualenv-init -)"
     fi
 fi
+
+# ruby [rbenv]
+if [ -d $HOME/.rbenv ]; then
+    if [ $uname != 'Darwin' ]; then
+        export PATH=$HOME/.rbenv/bin:$PATH
+    fi
+fi
 if _command_exists rbenv; then
     eval "$(rbenv init -)"
 fi
+
+# node.js [nodebrew]
+if [ -d $HOME/.nodebrew ]; then
+    export PATH=$HOME/.nodebrew/current/bin:$PATH
+fi
+
+# go [gvm]
+if [ -d $HOME/.gvm ]; then
+    source $HOME/.gvm/scripts/gvm
+fi
+
 #typeset -U path PATH
