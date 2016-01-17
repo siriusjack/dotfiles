8e493cafcd7beee6bea0a57b9f7ee4dd46c6ec85
siriusjack <siriusjack@gmail.com>
2016-01-18 01:32:19 +0900

    Ensure .cache/shell/chpwd-recent-dirs exists

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -2,6 +2,16 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=5000
 SAVEHIST=100000
+
+local hist_dir=$HOME/.cache/shell
+local hist_file=$hist_dir/chpwd-recent-dirs
+if [ ! -d $hist_dir ]; then
+    mkdir -p $hist_dir
+fi
+if [ ! -f $hist_file ]; then
+    touch $hist_file
+fi
+
 setopt hist_ignore_dups
 setopt share_history
 bindkey '^R' history-incremental-search-backward
